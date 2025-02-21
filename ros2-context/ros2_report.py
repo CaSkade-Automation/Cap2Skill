284c0ad676d5fbe5c4164f7e71b756ae9bb2cd98
Miguel Vieira <miguel.vieira@hsu-hh.de>
2025-02-21 17:16:09 +0100

    optimizing ros2 report generation

--- ros2-context/ros2_report.py
+++ ros2-context/ros2_report.py
@@ -5,12 +5,23 @@ import subprocess
 import datetime
 import time
 import logging
+from enum import Enum
+
+class Communication(Enum):
+    TOPICS = "topics"
+    SERVICES = "services"
+    ACTIONS = "actions"
+
+COMMUNICATION_FUNCTIONS ={
+    Communication.TOPICS: get_topic_names_and_types,
+    Communication.SERVICES: get_service_names_and_types,
+} 
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("ros2_inspector")
 
-def generate_report():
+def generate_report() -> None:
     rclpy.init()
     
     node = rclpy.create_node("ros2_inspector")
@@ -20,31 +31,10 @@ def generate_report():
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     report = f"# ROS2 System Report created on: {timestamp}"  
     
-    report += "\n\n## Topics\n"
-    # Topics
-    topics = get_stable_topics(node=node)
-    if not topics:
-        logger.warning("No active topics found")
-    report = write_communication_details(topics, report)
-    
-    report += "\n## Services\n"
-    # Services
-    services = get_stable_services(node=node)
-    if not services:
-        logger.warning("No active services found")
-    report = write_communication_details(services, report)
-    
-    report += "\n## Actions\n"
-    # Actions
-    actions = get_action_names_and_types_cli()
-    if not actions:
-        logger.warning("No active actions found")
-    logger.info("Number of actions: " + str(len(actions)))
-    for action_name, action_type in actions:
-        report += f"- **{action_name}**: (action_types)\n"
-        action_info = get_interface_details(action_type)
-        report += f"  - **Message Type Details:**\n{action_info}\n"
-
+    for comm_type in Communication:
+        report += f"\n## {comm_type.value.capitalize()}\n"
+        comm_list = get_stable_communications(node, comm_type)
+        report = write_interface_details(comm_list, report)
 
     # Save in file
     file_name = "ros2_system_report.md"
@@ -58,67 +48,69 @@ def generate_report():
 
     logger.info("ROS2 inspection finished.")
 
-def get_stable_topics(node, retries=5, delay=1.0):
-    """Retries topic discovery until the results stabilize."""
-    prev_result =[] 
-    for _ in range(retries):
-        topics = get_topic_names_and_types(node=node)
-        logger.info("Number of topics: " + str(len(topics)))
-        if topics == prev_result:  # If the results stabilize, return them
-            return topics
-        prev_result = topics
-        time.sleep(delay)
-    return prev_result  # Return last known stable state
+def get_stable_communications(node, communication_type: Communication, retries=5, delay=1.0) -> list[tuple[str,list[str]]] | list[tuple[str, str]]: 
+    """Retries interface discovery until the results stabilize."""
+    prev_result = [] 
 
-def get_stable_services(node, retries=5, delay=1.0):
-    """Retries topic discovery until the results stabilize."""
-    prev_result =[] 
-    for _ in range(retries):
-        topics = get_service_names_and_types(node=node)
-        logger.info("Number of services: " + str(len(topics)))
-        if topics == prev_result:  # If the results stabilize, return them
-            return topics
-        prev_result = topics
-        time.sleep(delay)
-    return prev_result  # Return last known stable state
+    get_communications = COMMUNICATION_FUNCTIONS.get(communication_type)
 
-def write_communication_details(communication_list, report):
-    for communication_name, communication_types in communication_list:
-        report += f"- **{communication_name}**:\n  - Type: {', '.join(communication_types)}\n"
-        for communication_type in communication_types:
-            communication_info = get_interface_details(communication_type)
-            report += f"  - **Type Details:**\n{communication_info}\n"
+    if get_communications is None: 
+        logger.error(f"Unknown communication type:{communication_type} ")
+        return []  
+
+    for _ in range(retries):
+        communications = get_communications(node=node)
+        
+        logger.info(f"Attempt {_+1} - Number of {communication_type.value}: " + str(len(communications)))
+        
+        if not communications:
+            logger.warning(f"No active {communication_type.value} found (Attempt {_+1}")
+
+        if communications == prev_result:  # If the results stabilize, return them
+            return communications
+        
+        prev_result = communications.copy()
+        time.sleep(delay)
+    return prev_result # Return last known stable state
+
+def write_interface_details(communication_list: list[tuple[str,list[str]]] | list[tuple[str, str]], report: str) -> str:
+    """Append communication details (interface) to the report."""
+    for communication_name, interfaces in communication_list:
+        report += f"- **{communication_name}**:\n  - Type: {', '.join(interfaces) if isinstance(interfaces, list) else interfaces}\n"
+        for interface in (interfaces if isinstance(interfaces, list) else [interfaces]):
+            report += f"  - **Type Details:**\n{get_interface_details(interface)}\n"
     return report
 
-def get_interface_details(interface_type):
+def get_interface_details(interface: str) -> str:
+    """Retrieve detailed information for a given ROS2 interface type."""
     try:
-        result = subprocess.run(["ros2", "interface", "show", interface_type], capture_output=True, text=True)
-        if result.returncode == 0:
-            return result.stdout.strip()
-        else:
-            logger.error(f"Error when retrieving the type {interface_type}: {result.stderr.strip()}")
-            return f"Error when retrieving the type: {result.stderr.strip()}"
+        result = subprocess.run(["ros2", "interface", "show", interface], capture_output=True, text=True)
+        return result.stdout.strip() if result else logger.error(f"Error when retrieving the type {interface}:{result.stderr.strip()}")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error retrieving type {interface}: {e.stderr.strip() if e.stderr else str(e)}")
     except Exception as e:
         logger.error(f"Error when retrieving the type: {str(e)}", exc_info=True)
-        return f"Error when retrieving the type: {str(e)}"
-
+    
 # not implemented from ros2 api: actions = get_action_names_and_types(node=node).     
-def get_action_names_and_types_cli():
+def get_action_names_and_types_cli(node) -> list[tuple[str, str] ] :
     try:
         result = subprocess.run(["ros2", "action", "list", "-t"], capture_output=True, text=True)
-        if result.returncode == 0:
-            actions = [] 
-            for line in result.stdout.strip().split("\n"):
-                parts = line.split(" ")
-                if len(parts) == 2:
-                    actions.append((parts[0], parts[1].lstrip("[").rstrip("]")))
-            return actions
-        else:
-            logger.error(f"Error when retrieving the actions: {result.stderr.strip()}")
-            return [] 
+        if not result: 
+            return[] 
+        actions = [] 
+        for line in result.stdout.strip().split("\n"):
+            parts = line.split(" ")
+            if len(parts) == 2:
+                actions.append((parts[0], parts[1].lstrip("[").rstrip("]")))
+        return actions
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error retrieving ros2 actions:{str(e)}") 
     except Exception as e:
-        logger.error(f"Error when retrieving the actions: {str(e)}", exc_info=True)
-        return [] 
+        logger.error(f"Unexpected error retrieving ros2 actions: {str(e)}", exc_info=True)
+    
+    return [] 
+    
+COMMUNICATION_FUNCTIONS[Communication.ACTIONS] = get_action_names_and_types_cli 
 
 if __name__ == "__main__":
     generate_report()
