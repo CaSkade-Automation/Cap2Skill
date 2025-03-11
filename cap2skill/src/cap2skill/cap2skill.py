e506b48eef1f65f724019f1f617a17df8c3233f3
Miguel Vieira <miguel_vieira@gmx.de>
2025-03-11 17:35:25 +0100

    testing method for retrieving relevant context

--- cap2skill/src/cap2skill/cap2skill.py
+++ cap2skill/src/cap2skill/cap2skill.py
@@ -37,7 +37,7 @@ class Cap2Skill:
         query = f"Find relevant {self.context_organizer.get_framework()} interfaces for the following capability: {self.capability}"
         #TODO: optimize the number of retrieved contexts
         retrieved_contexts = self.vector_store.similarity_search(query, 4) 
-        retrieved_control_entities = self.context_organizer.compare_control_entities([json.loads(retrieved_context.page_content) for retrieved_context in retrieved_contexts])
+        retrieved_control_entities = self.context_organizer.compare_control_entity_names([json.loads(retrieved_context.page_content) for retrieved_context in retrieved_contexts])
 
         return retrieved_control_entities
 
@@ -47,5 +47,28 @@ class Cap2Skill:
         chunk_documents = self.generate_chunks_from_context()
         self.set_vector_store(chunk_documents)
         retrieved_control_entities = self.retrieve_relevant_context()
-        skill = self.prompt_handler.prompt(PromptType.SKILL_GENERATION, {"language": self.language, "framework": self.context_organizer.get_framework(), "resource_type": self.context_organizer.get_resource_type(), "capability": self.capability, "control_entities": [retrieved_control_entity.to_json() for retrieved_control_entity in retrieved_control_entities]})
+        skill = self.prompt_handler.prompt(PromptType.SKILL_GENERATION, {"language": self.language, "framework": self.context_organizer.get_framework(), "resource_type": self.context_organizer.get_resource_type(), "capability": self.capability, "control_entities": [retrieved_control_entity.to_dict(include=["name", "type", "description"]) for retrieved_control_entity in retrieved_control_entities]})
         return skill
+    
+if __name__ == "__main__":
+    import os 
+    from prompt_handling.prompt_handling import load_file
+    from report_handling.ros2_handling import ROS2ReportHandling
+
+    context_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "../docs", "ros2_system_report_with_descriptions.json") # important: for testing only use of system reports with descriptions otherwise prompting is done for descriptions
+    ontology_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "../../capability-models", "set-velocity.ttl")
+    context = load_file(context_path)
+    ontology = load_file(ontology_path)
+
+    prompt_handler = PromptHandler()
+
+    control_entities_handler = ROS2ReportHandling(context, "ROS2", "Mobile Robot", prompt_handler)
+
+    cap2skill = Cap2Skill("Python", ontology, control_entities_handler, prompt_handler)
+    chunks = cap2skill.generate_chunks_from_context()
+    cap2skill.set_vector_store(chunks)
+    retrieved_control_entities = cap2skill.retrieve_relevant_context()
+    
+    file_name = "ros2_retrieved_context.txt"
+    with open(file_name, "w") as file:
+        json.dump([retrieved_control_entity.to_dict(include=["name", "type", "description"]) for retrieved_control_entity in retrieved_control_entities], file, indent=4)
