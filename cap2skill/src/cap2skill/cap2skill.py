e1cdd66b59115bd7b8581c77d0500f9f5b081892
Miguel Vieira <miguel_vieira@gmx.de>
2025-03-28 08:39:25 +0100

    Loading of ontology and reading rdfs:comment for use in RAG

--- cap2skill/src/cap2skill/cap2skill.py
+++ cap2skill/src/cap2skill/cap2skill.py
@@ -4,6 +4,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain.schema import Document
 from langchain_core.messages import BaseMessage
+from rdflib import Graph, Namespace, RDF, RDFS
 from report_handling.control_report_handling import ControlReportHandling, ControlEntity
 from prompt_handling.prompt_handling import PromptHandler, PromptType
 
@@ -14,16 +15,39 @@ class Cap2Skill:
         self.embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
         self.context_organizer = context_organizer
         self.language = language
-        self.capability = capability
+        self.capability_str = capability
+        self.capability_ont = self.read_ontology(capability)
         self.prompt_handler = prompt_handler
         self.vector_store = None
 
-    def generate_chunks_from_context(self) -> list[Document]:
-        chunks = []
+    def read_ontology(self, ontology: str) -> Graph:
+        """ Read the capability ontology. """
+        g = Graph()
+        g.parse(data=ontology, format="ttl")
+        return g
+    
+    def get_capability_description(self) -> str:
+        cask = Namespace("http://www.w3id.org/hsu-aut/cask#")
 
-        for control_entity in self.context_organizer.get_control_entities():
-            if self.context_organizer.get_framework() == "ROS2":
-                chunks.append(control_entity.to_json(include=["name", "type", "description"], interfaces = False))
+        capability_class = "ProvidedCapability"
+        capability_class_uri = cask[capability_class]
+
+        # Get the capability instance (only one capability per ontology)
+        capability = next(self.capability_ont.subjects(RDF.type, capability_class_uri), None)
+
+        if not capability:
+            raise ValueError(f"No capability found.")
+
+        comment = next(self.capability_ont.objects(capability, RDFS.comment), None)
+        
+        if not comment:
+            raise ValueError(f"No rdfs:comment -- description -- for {capability} found.")
+        
+        return str(comment)
+
+    def generate_chunks_from_context(self) -> list[Document]:
+
+        chunks = [control_entity.get_description() for control_entity in self.context_organizer.get_control_entities()]
 
         chunk_documents = [Document(page_content=text) for text in chunks]
         return chunk_documents 
@@ -34,10 +58,10 @@ class Cap2Skill:
     
     def retrieve_relevant_context(self) -> List[ControlEntity]: 
         """ Retrieve relevant context for the given capability ontology. """
-        query = f"Find relevant {self.context_organizer.get_framework()} interfaces for the following capability: {self.capability}"
+        query = f"Find {self.context_organizer.get_framework()} interfaces to implement the following capability: {self.get_capability_description()} "
         #TODO: optimize the number of retrieved contexts
         retrieved_contexts = self.vector_store.similarity_search(query, 4) 
-        retrieved_control_entities = self.context_organizer.compare_control_entity_names([json.loads(retrieved_context.page_content) for retrieved_context in retrieved_contexts])
+        retrieved_control_entities = self.context_organizer.compare_control_entity_descriptions([retrieved_context.page_content for retrieved_context in retrieved_contexts])
 
         return retrieved_control_entities
 
@@ -62,7 +86,7 @@ if __name__ == "__main__":
 
     prompt_handler = PromptHandler()
 
-    control_entities_handler = ROS2ReportHandling(context, "ROS2", "Mobile Robot", prompt_handler)
+    control_entities_handler = ROS2ReportHandling("ROS2", "Mobile Robot", prompt_handler, context)
 
     cap2skill = Cap2Skill("Python", ontology, control_entities_handler, prompt_handler)
     chunks = cap2skill.generate_chunks_from_context()
@@ -71,4 +95,4 @@ if __name__ == "__main__":
     
     file_name = "ros2_retrieved_context.txt"
     with open(file_name, "w") as file:
-        json.dump([retrieved_control_entity.to_dict(include=["name", "type", "description"]) for retrieved_control_entity in retrieved_control_entities], file, indent=4)
+        json.dump([retrieved_control_entity.to_dict() for retrieved_control_entity in retrieved_control_entities], file, indent=4)
