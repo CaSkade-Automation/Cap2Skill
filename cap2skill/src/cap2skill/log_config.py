371b44d58eddeef8e34c044ae9aa86d9acdcfaac
Miguel Vieira <miguel_vieira@gmx.de>
2025-03-28 08:40:44 +0100

    log config for better logging

--- cap2skill/src/cap2skill/log_config.py
+++ cap2skill/src/cap2skill/log_config.py
@@ -1,35 +1,45 @@
 import logging
 import colorlog
 
-# Remove existing handlers
-for handler in logging.root.handlers[:]:
-    logging.root.removeHandler(handler)
+def setup_logger():
+    # Remove existing handlers
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
 
-# ANSI escape codes for text styling
-BOLD = "\033[1m"
-UNDERLINE = "\033[4m"
-RED = "\033[31m"
-RESET = "\033[0m"
+    # ANSI escape codes for text styling
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    RED = "\033[31m"
+    RESET = "\033[0m"
 
-# Define log colors
-log_colors = {
-    "DEBUG": "cyan",
-    "INFO": "green",
-    "WARNING": "yellow",
-    "ERROR": "red",
-    "CRITICAL": "bold_red",
-}
+    # Define log colors
+    log_colors = {
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    }
 
-# Define log formatter
-formatter = colorlog.ColoredFormatter(
-    "%(blue)s[%(asctime)s]%(reset)s %(purple)s[%(name)s]%(reset)s %(log_color)s[%(levelname)s]:%(reset)s %(message)s",
-    log_colors=log_colors,
-    reset=True,
-)
+    # Define log formatter
+    formatter = colorlog.ColoredFormatter(
+        "%(blue)s[%(asctime)s]%(reset)s %(purple)s[%(name)s]%(reset)s %(log_color)s[%(levelname)s]:%(reset)s %(message)s",
+        log_colors=log_colors,
+        reset=True,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
-# Create a console handler with color formatting
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
+    # Create a console handler with color formatting
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, handlers=[console_handler])
+# logging.basicConfig(level=logging.INFO, handlers=[console_handler])
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(console_handler)
+
+# Call this once at the beginning
+setup_logger()
+
+logger = logging.getLogger(__name__)
