09830b0c1b87ece192e149c56578063c16359dfb
Miguel Vieira <miguel_vieira@gmx.de>
2025-03-31 19:30:27 +0200

    Expansion of get-position skill with pySkillUp. Additionally, necessary information about skill states is added.

--- skill-implementations/src/get_position/get_position/get_position.py
+++ skill-implementations/src/get_position/get_position/get_position.py
@@ -1,19 +1,42 @@
+from pyskillup.decorators.decorators import *
+from pyskillup.decorator_check.decorator_check import *
+
+from pyskillup.decorators.skill_interface import SkillInterface
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry
 
-
+@skill(skill_interface = SkillInterface.REST, skill_iri="http://www.hsu-hh.de/aut/ontologies/mobile-robots#neobotix-mmo-700/skills/get-position", module_iri= "http://www.hsu-hh.de/aut/ontologies/mobile-robots#neobotix-mmo-700/skills/get-position", capability_iri = "http://www.hsu-hh.de/aut/ontologies/mobile-robots#neobotix-mmo-700/capabilities/get-position", description = "Retrieve the current position of the mobile robot.")
 class GetPositionSkill(Node): 
 
     def __init__(self):
         super().__init__('get_position')
+        self.position = Point()
+
+    @skill_output(is_required=True, name="position", description="The position of the robot.")
+    def get_position(self) -> Point:
+        return self.position
+
+    @starting
+    def starting(self) -> None:
+        self.get_logger().info("GetPositionSkill is starting with subscription to odom topic")
         self.odom_subscription = self.create_subscription(Odometry, "odom", self.odom_listener_callback, 10)
 
     def odom_listener_callback(self, msg: Odometry) -> None: 
-        position = Point()
-        position = msg.pose.pose.position
-        self.get_logger().info(f'Position: x={position.x}, y={position.y}')
+        self.position = msg.pose.pose.position
+        self.get_logger().info(f'Position: x={self.position.x}, y={self.position.y}')
+
+    @execute
+    def execute(self) -> Point: 
+        self.get_logger().info(f'Position: x={self.position.x}, y={self.position.y}')
+        return self.position
+    
+    @completing
+    def stopping(self) -> None:
+        self.get_logger().info("GetPositionSkill is completing")
+        self.odom_subscription.destroy()
 
 def main(args=None):
     rclpy.init(args=args)
