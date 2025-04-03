de1c5dfdb480b911e71adc448b4904887fa688d2
Miguel Vieira <miguel_vieira@gmx.de>
2025-04-03 13:31:27 +0200

    Correction of set-velocity. Skill Ouptut was missing

--- skill-implementations/set_velocity.py
+++ skill-implementations/set_velocity.py
@@ -1,7 +1,9 @@
+import time
 from pyskillup.decorators.decorators import *
 from pyskillup.decorator_check.decorator_check import ROS2Skill
 from pyskillup.decorators.skill_interface import SkillInterface
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 @skill(skill_interface = SkillInterface.REST, skill_iri="http://www.hsu-hh.de/aut/ontologies/mobile-robots/neobotix/mmo700/skills/setVelocity/setVelocity", module_iri= "http://www.hsu-hh.de/aut/ontologies/mobile-robots/neobotix/mmo700/neobotixMMO700", capability_iri = "http://www.hsu-hh.de/aut/ontologies/mobile-robots/neobotix/mmo700/capabilities/setVelocity/setVelocity", description = "Set the velocity of the mobile robot in the forward direction.")
 class SetVelocitySkill(ROS2Skill): 
@@ -13,10 +15,17 @@ class SetVelocitySkill(ROS2Skill):
 
         self.velocity_publisher = None
         self.velocity_cmd = Twist()
+        self.velocity_robot = Twist()
+        self.odom_subscription = None
+        self.received_odom = False
 
-    @skill_parameter(is_required=True, name="velocity", description="The desired velocity of the robot.")
-    def get_velocity(self) -> float:
+    @skill_parameter(is_required=True, name="desired_velocity", description="The desired velocity of the robot.")
+    def get_desired_velocity(self) -> float:
         return self.velocity_cmd.linear.x
+    
+    @skill_output(is_required=True, name="output_velocity", description="The current velocity of the robot.")
+    def get_output_velocity(self) -> float:
+        return self.velocity_robot.linear.x
 
     @starting
     def starting(self) -> None:
@@ -32,6 +41,21 @@ class SetVelocitySkill(ROS2Skill):
         self.velocity_publisher.publish(self.velocity_cmd)
         self.node.get_logger().info(f'Publishing velocity: {self.velocity_cmd.linear.x}')
 
+    @completing
+    def completing(self) -> None:
+        self.node.get_logger().info("SetVelocitySkill is completing")
+        self.odom_subscription = self.node.create_subscription(Odometry, "odom", self.odom_listener_callback, 10)
+        while not self.received_odom:
+            self.node.get_logger().info("Waiting for odometry data...")
+            time.sleep(0.1)
+        
+        self.node.get_logger().info(f'Completing SetVelocitySkill with velocity: {self.velocity_robot.linear.x}')
+
+    def odom_listener_callback(self, msg: Odometry) -> None: 
+        self.velocity_robot = msg.twist.twist
+        self.received_odom = True
+        self.node.get_logger().info(f'Received velocity: {self.velocity_robot.linear.x}')
+
     @stopping
     def stopping(self) -> None:
         self.node.get_logger().info("SetVelocitySkill is stopping")
@@ -43,3 +67,5 @@ class SetVelocitySkill(ROS2Skill):
     def resetting(self) -> None:
         self.node.get_logger().info("SetVelocitySkill is resetting")
         self.velocity_publisher.destroy()
+        self.odom_subscription.destroy()
+        self.received_odom = False
